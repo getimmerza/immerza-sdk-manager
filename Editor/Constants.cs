5d183db6dd43d9891c4d10cd431d0b7a6b0eaf01
trauty <trautwein.maximilian04@gmail.com>
2025-03-27 19:24:56 +0100

    added auth and tab system

---
+++ Editor/Constants.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace ImmerzaSDK.Manager.Editor
+{
+    internal static class Constants
+    {
+        internal const string API_BASE_URL = "https://api.ovok.com/";
+        internal const string CLIENT_ID = "fc86ca24-e854-4c7f-bde1-fd5bb04d9a6d";
+    }
+}
