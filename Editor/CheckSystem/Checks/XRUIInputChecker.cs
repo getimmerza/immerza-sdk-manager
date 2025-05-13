8b510630d2a7199a9e4ab7b3cfdac4215df22956
trauty <trautwein.maximilian04@gmail.com>
2025-05-13 18:40:29 +0200

    1.0.3 - fixed near far interactor check

--- Editor/CheckSystem/Checks/XRUIInputChecker.cs
+++ Editor/CheckSystem/Checks/XRUIInputChecker.cs
@@ -17,31 +17,11 @@ public class XRUIInputChecker : ICheckable
 
         if (found.Length == 0)
         {
-            context.AddError("No NearFarInteractors found! Scene needs atleast two set to the 'UI' layer.");
+            context.AddError("No NearFarInteractors found! Scene needs atleast one for each hand.");
         }
         else if (found.Length == 1)
         {
-            if ((found[0].interactionLayers.value & InteractionLayerMask.GetMask("UI")) == 0)
-            {
-                context.AddError("Not one NearFarInteractor with the 'UI' interaction layer has been found! Scene needs atleast two set to the 'UI' layer.");
-            }
-        }
-        else
-        {
-            int foundInteractors = 0;
-
-            foreach (NearFarInteractor interactor in found)
-            {
-                if ((interactor.interactionLayers.value & InteractionLayerMask.GetMask("UI")) != 0)
-                {
-                    foundInteractors++;
-                }
-            }
-
-            if (foundInteractors < 2)
-            {
-                context.AddError("Only one NearFarInteractor with the 'UI' interaction layer has been found! Scene needs atleast two set to the 'UI' layer.", found[0]);
-            }
+            context.AddError("Only one NearFarInteractor found! Scene needs atleast one for each hand.");
         }
     }
 }
